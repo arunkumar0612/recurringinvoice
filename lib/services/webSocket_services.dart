e553f3b8e46a18617bf3b9285ecbcb21c06914a2
HARI <hariprasath.s@sporadasecure.com>
2025-04-19 17:58:09 +0530

    sds

--- lib/services/webSocket_services.dart
+++ lib/services/webSocket_services.dart
@@ -1,10 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 
 mixin WebsocketServices {
   static void startWebSocketServer() async {
     final server = await HttpServer.bind(InternetAddress.anyIPv4, 8081);
     print('WebSocket server running on ws://${server.address.address}:8081');
-
+    List<Map<String, dynamic>> allSites = [];
     await for (HttpRequest request in server) {
       if (WebSocketTransformer.isUpgradeRequest(request)) {
         WebSocket socket = await WebSocketTransformer.upgrade(request);
@@ -12,9 +13,21 @@ mixin WebsocketServices {
 
         socket.listen(
           (message) {
-            print('Received: $message');
-            // Generators.InvoiceGenerator(message);
-            socket.add('Echo: $message');
+            // print('Received: $message');
+
+            try {
+              final decoded = jsonDecode(message);
+
+              if (decoded is List) {
+                allSites = decoded.map<Map<String, dynamic>>((item) => Map<String, dynamic>.from(item)).toList();
+                print('Ressssssssssssssssssceived: $allSites');
+              } else {
+                print('Decoded message is not a list');
+              }
+            } catch (e) {
+              print('Error decoding JSON: $e');
+            }
+            // print('Received: $message');
           },
           onDone: () {
             print('Client disconnected');
